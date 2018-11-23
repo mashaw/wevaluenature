30824240fcd0a5c7ba9df70fdb4e66e7524dbbff
Mike Shaw <mikeshaw@Mikes-MBP-2.countryscape.org>
2018-11-23 18:00:53 +0000

    update

--- js/scroll.js
+++ js/scroll.js
@@ -1,82 +1,70 @@
-(function ($) {
-		// This jQuery function is called when the document is ready
-		$(function () {
-	
-			// Set scroll speed
-			var speed = 1000;
 
-			$('a.jumpTo3').click(function () {
-				$('html, body').animate({
-					scrollTop: $('.node-3').offset().top
-				}, speed, 'swing');
-			});
-			$('a.jumpTo4').click(function () {
-				$('html, body').animate({
-					scrollTop: $('.node-4').offset().top
-				}, speed, 'swing');
-			});
-			$('a.jumpTo16').click(function () {
-				$('html, body').animate({
-					scrollTop: $('.node-16').offset().top
-				}, speed, 'swing');
-			});
-			$('a.jumpTo18').click(function () {
-				$('html, body').animate({
-					scrollTop: $('.node-18').offset().top
-				}, speed, 'swing');
-			});
-			
-			
-			$(window).scroll(function () {
-			
-				var windowTop = $(window).scrollTop();
-				var windowTop = windowTop += 10;
-  				
-				if ($('.node-3').offset().top < windowTop) {
-					$('#page').removeClass();
-					$('#page').addClass('jumpTo3active');
-				}
-					
-				if ($('.node-4').offset().top < windowTop) {
-					$('#page').removeClass();
-					$('#page').addClass('jumpTo4active');
-				}
-				
-				if ($('.node-16').offset().top < windowTop) {
-					$('#page').removeClass();
-					$('#page').addClass('jumpTo16active');
-				}
-					
-				if ($('.node-18').offset().top < windowTop) {
-					$('#page').removeClass();
-					$('#page').addClass('jumpTo18active');
-				}			
-			});
-						
-			$(window).resize(function () {
+/**
+ * @file
+ * Attaches the behaviors for the Scroll to Destination Anchors module.
+ */
 
-				var windowTop = $(window).scrollTop();
-				
-				if ($('.node-3').offset().top < windowTop) {
-					$('#page').removeClass();
-					$('#page').addClass('jumpTo3active');
-				}
-				
-				if ($('.node-4').offset().top < windowTop) {
-					$('#page').removeClass();
-					$('#page').addClass('jumpTo4active');
-				}
-					
-				if ($('.node-16').offset().top < windowTop) {
-					$('#page').removeClass();
-					$('#page').addClass('jumpTo16active');
-				}
-				if ($('.node-18').offset().top < windowTop) {
-					$('#page').removeClass();
-					$('#page').addClass('jumpTo18active');
-				}
+// Prevent script conflicts and attach the behavior.
+(function($) {
+  Drupal.behaviors.scrolltoanchors = {
+    attach: function(context, settings) {
 
-			});
-			
-});
-	})(jQuery);
+      // Wait until after the window has loaded.
+      $(window).load(function(){
+        // Utility to check if a string is a valid selector.
+        function validateSelector(a) {
+          return /^#[a-z]{1}[a-z0-9_-]*$/i.test(a);
+        }
+
+        // Utility to scroll users to particular destination on the page.
+        function scrollToDestination(a, b) {
+          if (a > b) {
+            destination = b;
+          } else {
+            destination = a;
+          }
+          var movement = 'scroll mousedown DOMMouseScroll mousewheel keyup';
+          $('html, body').animate({scrollTop: destination}, 500, 'swing').bind(movement, function(){
+            $('html, body').stop();
+          });
+        }
+
+        // When a user clicks on a link that starts with a hashtag.
+        $('a[href^="#"]', context).click(function(event) {
+          block-bartik-powered("click event");
+
+          // Store important values.
+          var hrefValue = $(this).attr('href');
+          var strippedHref = hrefValue.replace('#','');
+          var heightDifference = $(document).height() - $(window).height();
+
+          // Make sure that the link value is a valid selector.
+          if (validateSelector(hrefValue)) {
+
+            // Scroll users if there is an element with a corresponding id.
+            if ($(hrefValue).length > 0) {
+              var linkOffset = $(this.hash).offset().top;
+              scrollToDestination(linkOffset, heightDifference);
+            }
+
+            // Scroll users if there is a link with a corresponding name.
+            else if ($('a[name=' + strippedHref + ']').length > 0) {
+              var linkOffset = $('a[name=' + strippedHref + ']').offset().top;
+              scrollToDestination(linkOffset, heightDifference);
+            }
+
+            // Add the href value to the URL.
+            document.location.hash = strippedHref;
+
+          }
+
+          // Prevent the event's default behavior.
+          event.preventDefault();
+
+        });
+
+      });
+
+    }
+  };
+}(jQuery));
